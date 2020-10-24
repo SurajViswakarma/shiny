5a1926d141b4c735454d6902dc1fb971593238e8
Suraj Viswakarma <44229984+SurajViswakarma@users.noreply.github.com>
2020-10-24 23:50:10 +0530

    Update init.R

--- init.R
+++ init.R
@@ -2,11 +2,12 @@
 #
 #Example R code to install packages if n ot already installed
 
-my_packages	c ("randomForest" , " data . table"
+my_packages =	c("randomForest", "data.table")
 
-install if_missing = function(p) { if (p %in% rownames (installed . packages( ) ) FALSE
+install_if_missing = function(p) { if (p %in% rownames (installed.packages( ) ) == FALSE){
 
 install. packages (p)
-
+}
+                                  }
 invisible(sapply(my_packages, install_if_missing))
 
